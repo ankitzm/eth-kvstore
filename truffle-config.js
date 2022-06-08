dd1a15b3e5f1be2bededd9c93eb50e84d3345f56
eugenvoronov <voronovege@cy-tech.fr>
2022-06-08 16:50:40 +0200

    Updated config files

---
+++ truffle-config.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+const { INFURA_TOKEN, MNEMONIC, ETH_HOST, ETH_PORT } = process.env;
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+module.exports = {
+  networks: {
+    development: {
+      host:  ETH_HOST || '127.0.0.1',
+      port: ETH_PORT || 7545,
+      network_id: '*',
+    },
+    live: {
+      provider: () => new HDWalletProvider(MNEMONIC, `https://mainnet.infura.io/v3/${INFURA_TOKEN}`),
+      network_id: '1',
+    },
+    kovan: {
+      provider: () => new HDWalletProvider(MNEMONIC, `https://kovan.infura.io/v3/${INFURA_TOKEN}`),
+      network_id: '42',
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(MNEMONIC, `https://ropsten.infura.io/v3/${INFURA_TOKEN}`),
+      network_id: '3',
+      gas: 4700000,
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(MNEMONIC, `https://rinkeby.infura.io/v3/${INFURA_TOKEN}`),
+      network_id: '4',
+    },
+  },
+  compilers: {
+    solc: {
+        version: "0.8.2",
+    }
+  }
+};
